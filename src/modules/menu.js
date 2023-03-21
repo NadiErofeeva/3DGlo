08576fe910dd2aa4b15cdb20beae37acea57cd0f
Nadi Erofeeva <n.erofeeva72@gmail.com>
2023-03-21 17:39:35 +0500

    lesson 22 & hard

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,24 +1,21 @@
 const menu = () => {
-    const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
-    //const closeBtn = menu.querySelector('.close-btn');
-    //const menuItems = menu.querySelectorAll('ul>li>a');
+    const body = document.querySelector('body');
 
     const handleMenu = () => {
         menu.classList.toggle('active-menu')
     }
 
-    menuBtn.addEventListener('click', handleMenu);
-
-    menu.addEventListener('click', (e) => {
-        //console.log(e.target.classList.contains('close-btn'))
-        if (e.target.tagName === 'A' ) {
+    body.addEventListener('click', (e) => {
+        if (e.target.closest('.menu') || e.target.closest('.close-btn')) {
+            handleMenu()
+        }
+        else if (!e.target.closest('menu')) {
+            menu.classList.remove('active-menu')
+        } else if (e.target.matches('menu ul a')) {
             handleMenu()
         }
     })
-
-    //closeBtn.addEventListener('click', handleMenu);
-    //menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
 }
 
 export default menu
